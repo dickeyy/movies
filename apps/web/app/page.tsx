f2f5b63d5475b5dbd59178d2d374a489b15c08fa
Kyle Dickey <k@kyle.so>
2025-01-05 23:30:21 -0700

    add footer

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,33 @@
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen justify-center items-center px-4 py-12 sm:px-6 lg:px-8">
+        <div className="flex flex-col min-h-screen items-center">
             <Navbar active="home" />
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <div className="flex flex-col space-y-2 items-start justify-center">
-                    <p className="text-foreground/70 text-sm">movies.kyle.so</p>
-                    <h1 className="text-2xl font-medium">
-                        A free and open-source{" "}
-                        <span>
-                            <Link
-                                href="https://letterboxd.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="underline text-primary underline-offset-4 decoration-foreground/20 hover:decoration-foreground transition-colors"
-                            >
-                                Letterboxd
-                            </Link>
-                        </span>{" "}
-                        alternative.
-                    </h1>
+            <main className="flex-1 flex flex-col items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+                <div className="flex flex-col gap-8 items-center sm:items-start">
+                    <div className="flex flex-col space-y-2 items-start justify-center">
+                        <p className="text-foreground/70 text-sm">movies.kyle.so</p>
+                        <h1 className="text-2xl font-medium">
+                            A free and open-source{" "}
+                            <span>
+                                <Link
+                                    href="https://letterboxd.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="underline text-primary underline-offset-4 decoration-foreground/20 hover:decoration-foreground transition-colors"
+                                >
+                                    Letterboxd
+                                </Link>
+                            </span>{" "}
+                            alternative.
+                        </h1>
+                    </div>
                 </div>
             </main>
+            <Footer />
         </div>
     );
 }
---
+++ apps/web/components/footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer className="pb-2 px-4 pt-0 text-center text-sm shadow-lg w-screen flex flex-row justify-between items-center">
+            <p className="text-foreground/40 text-xs">
+                © {new Date().getFullYear()}{" "}
+                <Link href="https://kyle.so" className="hover:underline">
+                    Kyle Dickey
+                </Link>
+            </p>
+        </footer>
+    );
+}
