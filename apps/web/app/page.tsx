580fdc4ae200eac687eebb57279c8fc3af774965
Kyle Dickey <k@kyle.so>
2025-01-06 19:01:46 -0700

    redesign home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,21 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
+    const comingSoon = true;
     return (
         <div className="flex min-h-screen flex-col items-center">
-            <Navbar active="home" />
+            {!comingSoon && <Navbar active="home" />}
             <main className="flex flex-1 flex-col items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-                <div className="flex flex-col items-center gap-8 sm:items-start">
+                <div className="flex flex-col items-center sm:items-start">
                     <div className="flex flex-col items-start justify-center space-y-2">
-                        <p className="text-sm text-foreground/70">movies.kyle.so</p>
+                        <div className="flex w-full flex-row items-center justify-between font-mono text-sm text-foreground/70">
+                            <p>movies.kyle.so</p>
+                            <p>Coming Soon</p>
+                        </div>
                         <h1 className="text-2xl font-medium">
                             A free and open-source{" "}
                             <span>
@@ -26,6 +32,16 @@ export default function Home() {
                         </h1>
                     </div>
                 </div>
+                <Button className="group mt-8 w-full" asChild>
+                    <Link
+                        href="https://bsky.app/profile/kyle.so"
+                        target="_blank"
+                        className="flex items-center"
+                    >
+                        Get Updates
+                        <ArrowRightIcon className="ml-0 h-2 w-2 translate-x-[-5px] transform opacity-0 transition-all duration-150 ease-in-out group-hover:ml-1 group-hover:translate-x-0 group-hover:opacity-100" />
+                    </Link>
+                </Button>
             </main>
             <Footer />
         </div>
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
     darkMode: ["class"],
@@ -9,6 +10,9 @@ export default {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                mono: ["CommitMono", ...defaultTheme.fontFamily.mono]
+            },
             colors: {
                 background: "hsl(var(--background))",
                 foreground: "hsl(var(--foreground))",
